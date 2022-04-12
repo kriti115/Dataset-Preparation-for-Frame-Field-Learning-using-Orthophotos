8f8a61c3e867aa16b231dc72aed0da28dfe2ad3d
Kriti Shrestha <60517504+kriti115@users.noreply.github.com>
2022-04-12 12:43:16 +0200

    Create rename.py
    Problem: The downloaded images will have different names based on the extent of image. 
    
    Solution: We want to name them according to the city they belong to and assign a number to them.

---
+++ rename.py
@@ -0,0 +1,29 @@
+# Rename image files: folderwise
+
+import os
+
+def rename(path):
+    folder = os.listdir(path) # list of folders inside tif i.e. city named folders
+
+    for i in range(len(folder)):
+        #print(folder[i]) # folder with city name
+        city_name = folder[i]
+        #print(city_name)
+        city_fold = os.path.join(path, folder[i])
+        #print(city_fold) # path till folder with city name
+        count = 1
+        for file_name in os.listdir(city_fold):
+            #print(file_name) # the files inside the city name folder i.e tifs
+            old = os.path.join(city_fold, file_name)
+            new = os.path.join(city_fold, '{}{}.tif'.format(city_name, count))
+            if os.path.isfile(new):
+                continue
+            else:
+                os.rename(old,new)
+            count += 1
+
+def main():
+    binary = rename('data/images')
+    
+if __name__ == "__main__":
+    main()
