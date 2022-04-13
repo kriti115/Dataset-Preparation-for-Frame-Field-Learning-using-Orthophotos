5df78cbe78a2b57e36f2f5c65dc139da3f8ed1e0
Kriti Shrestha <60517504+kriti115@users.noreply.github.com>
2022-04-13 13:49:03 +0200

    Create get_shp_dir.py

---
+++ get_shp_dir.py
@@ -0,0 +1,31 @@
+''' Utilities: helping functions that are used in one or many of the scripts '''
+
+#from osgeo import gdal
+import os
+
+def get_dir_shp(path_shp):
+    fold = os.listdir(path_shp) 
+    folder = sorted(fold)
+    path_to_name = []
+    name_shp = []
+    cities = []
+
+    for i in range(len(folder)):
+        city_name = folder[i]
+        cities.append(city_name)
+        city_fold = os.path.join(path_shp, folder[i])
+        for file_name in os.listdir(city_fold):
+            if file_name.endswith('.shp'):
+                img_name, _ = os.path.splitext(file_name)
+                im = os.path.join(city_fold,file_name)
+                path_to_name.append(im)
+                name_shp.append(img_name)
+
+    #print(name_shp, path_to_name)
+    return name_shp, path_to_name
+
+def main():
+    bounding = get_dir_shp('data/shapefile')
+    
+if __name__ == "__main__":
+    main() 
