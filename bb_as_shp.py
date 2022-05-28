91629a3aec4e931c6f96ca7b601c5ac30899869b
Kriti Shrestha <kritishrestha523@gmail.com>
2022-05-29 00:50:51 +0200

    Update bb_as_shp.py

--- bb_as_shp.py
+++ bb_as_shp.py
@@ -22,17 +22,19 @@ def bb_as_shp(path_to_image):
     
     bound_box = []
     for i in range(len(image_path)):
+        bound_box_each = []
         for j in range(len(image_path[i])):
-            print(image_path[i][j])
+            #print(image_path[i][j])
             bb = boundingBox(image_path[i][j])
-            bound_box.append(bb)
+            bound_box_each.append(bb)
+        bound_box.append(bound_box_each)
     
     createFolder('./data/bounding_box/')
     bb = []
     for i in range(len(image_path)): # 4
         createFolder('./data/bounding_box/{}/'.format(cities[i])) 
         for j in range(len(image_path[i])): 
-            bb = shapely.geometry.box(*bound_box[j], ccw=True)
+            bb = shapely.geometry.box(*bound_box[i][j], ccw=True)
             print(bb)
             gpd.GeoDataFrame(pd.DataFrame(['p1'], columns = ['geom']),crs = {'init': 'epsg:25832'},
              geometry = [bb]).to_file('data/bounding_box/{}/{}.shp'.format(cities[i], image_name[i][j]))
